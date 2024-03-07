c987838512416717379c8fb7eb1e54d3833c9ea2
mvalespinoza <valerie.em.2004@gmail.com>
2024-03-07 17:31:05 -0600

    push and get tareas

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI(
 )
 
 users = []
-
+tasks = []
 
 @app.post("/register")
 async def create_user(username: str, correo: str, password: str):
@@ -36,5 +36,32 @@ async def get_user(user_id: int):
             return user
     raise HTTPException(status_code=404, detail="User not found")
 
+@app.post("/tasks/create")
+async def create_task(title: str, description: str, status: str, user_id: int):
+    task_data = {
+        "id": len(tasks) + 1,
+        "title": title,
+        "description": description,
+        "status": status,
+        "user_id": user_id
+    }
+    tasks.append(task_data)
+    return {
+        "id": task_data["id"],
+        "title": title,
+        "description": description,
+        "status": status,
+        "user_id": user_id,
+        "message": "The task was created successfully",
+        "status_code": 201
+    }
+
+@app.get("/tasks/{user_id}")
+async def get_tasks(user_id: int):
+    user_tasks = [task for task in tasks if task["user_id"] == user_id]
+    if not user_tasks:
+        raise HTTPException(status_code=404, detail="No tasks found for the user")
+    return user_tasks
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8002)
